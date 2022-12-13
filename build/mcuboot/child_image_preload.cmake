14308d2db16aaab3343d8c9a97a2583a757c86ba
jforgue <26842354+bandogora@users.noreply.github.com>
2022-12-13 11:19:41 -0500

    Update gitignore

--- build/mcuboot/child_image_preload.cmake
+++
@@ -1,7 +0,0 @@
-# Generated file used for preloading a child image
-set(CMAKE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(CMAKE_VERBOSE_MAKEFILE "FALSE" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_DIR "/home/jforgue/zephyr-cell-new/zephyr/boards/arm/circuitdojo_feather_nrf9160" CACHE INTERNAL "NCS child image controlled")
-set(ZEPHYR_TOOLCHAIN_VARIANT "zephyr" CACHE INTERNAL "NCS child image controlled")
-set(BOARD "circuitdojo_feather_nrf9160" CACHE INTERNAL "NCS child image controlled")
-set(OVERLAY_CONFIG "/home/jforgue/zephyr-cell-new/nrf/modules/mcuboot/tfm.conf;/home/jforgue/zephyr-cell-new/nrf/subsys/partition_manager/partition_manager_enabled.conf" CACHE INTERNAL "NCS child image controlled")
--- build/mcuboot/shared_vars.cmake
+++
@@ -1,8 +0,0 @@
-add_custom_target(mcuboot_shared_property_target)
-set_property(TARGET mcuboot_shared_property_target  PROPERTY KERNEL_HEX_NAME;zephyr.hex)
-set_property(TARGET mcuboot_shared_property_target  PROPERTY ZEPHYR_BINARY_DIR;/home/jforgue/zephyr-cell-new/build/mcuboot/zephyr)
-set_property(TARGET mcuboot_shared_property_target  PROPERTY KERNEL_ELF_NAME;zephyr.elf)
-set_property(TARGET mcuboot_shared_property_target  PROPERTY BUILD_BYPRODUCTS;/home/jforgue/zephyr-cell-new/build/mcuboot/zephyr/zephyr.hex;/home/jforgue/zephyr-cell-new/build/mcuboot/zephyr/zephyr.elf)
-set_property(TARGET mcuboot_shared_property_target  PROPERTY SIGNATURE_KEY_FILE;root-ec-p256.pem)
-set_property(TARGET mcuboot_shared_property_target APPEND PROPERTY PM_YML_DEP_FILES;/home/jforgue/zephyr-cell-new/bootloader/mcuboot/boot/zephyr/pm.yml)
-set_property(TARGET mcuboot_shared_property_target APPEND PROPERTY PM_YML_FILES;/home/jforgue/zephyr-cell-new/build/mcuboot/zephyr/include/generated/pm.yml)
