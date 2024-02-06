d0d14f3ae1b8455cd58bccf12e869df117e75484
jforgue <26842354+bandogora@users.noreply.github.com>
2024-02-06 09:01:47 -0500

    Add update_stop files and timer

---
+++ app/src/update_stop.h
@@ -0,0 +1,32 @@
+#ifndef UPDATE_STOP_H
+#define UPDATE_STOP_H
+
+#include <zephyr/kernel.h>
+
+/** Specify the number of display boxes connected*/
+#define NUMBER_OF_DISPLAY_BOXES 5
+
+/** Specify the route, display text, and position for each display box */
+// clang-format off
+#define DISPLAY_BOXES {                                  \
+  { .id = 20038, .position = 0, .direction_code = 'S' }, \
+  { .id = 30043, .position = 1, .direction_code = 'E' }, \
+  { .id = 30043, .position = 2, .direction_code = 'W' }, \
+  { .id = 30943, .position = 3, .direction_code = 'W' }, \
+  { .id = 10029, .position = 4, .direction_code = 'S' }  \
+}
+// clang-format on
+
+typedef const struct DisplayBox {
+  const char direction_code;
+  const int id;
+  const int position;
+} DisplayBox;
+
+void update_stop_timeout_handler(struct k_timer* timer_id);
+int update_stop(void);
+
+extern struct k_timer update_stop_timer;
+extern struct k_sem stop_sem;
+
+#endif
