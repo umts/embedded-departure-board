7e22ac90e657799cb6ae9596578debca0cc66c5f
jforgue <26842354+bandogora@users.noreply.github.com>
2024-06-14 18:20:17 -0400

    Add debugging options and config values

--- app/src/fota.h
+++ app/src/fota.h
@@ -5,20 +5,6 @@
 #ifndef FOTA_H
 #define FOTA_H
 
-// #include <zephyr/stats/stats.h>
-
-// /* Define an example stats group; approximates seconds since boot. */
-// STATS_SECT_START(smp_svr_stats)
-// STATS_SECT_ENTRY(ticks)
-// STATS_SECT_END;
-
-// /* Assign a name to the `ticks` stat. */
-// STATS_NAME_START(smp_svr_stats)
-// STATS_NAME(smp_svr_stats, ticks)
-// STATS_NAME_END(smp_svr_stats);
-
-// /* Define an instance of the stats group. */
-// extern STATS_SECT_DECL(smp_svr_stats) smp_svr_stats;
 #include <zephyr/types.h>
 
 int write_buffer_to_flash(char *data, size_t len, _Bool flush);
