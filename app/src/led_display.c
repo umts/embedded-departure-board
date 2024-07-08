92def68c2d5e241a35ab484596c803b38eb3e4e6
jforgue <26842354+bandogora@users.noreply.github.com>
2024-07-08 11:51:25 -0400

    Pull display changes for bit_bang_ws2813b branch
    - Update led_display code for non-daisy-chained displays
    - Added mux driver port select to write_num_to_display function

--- app/src/led_display.c
+++ app/src/led_display.c
@@ -1,16 +1,24 @@
-#include <led_display.h>
-#include <sys/_stdint.h>
+#include "led_display.h"
+
+#include <drivers/multiplexer/multiplexer.h>
+#include <zephyr/device.h>
 #include <zephyr/drivers/led_strip.h>
 #include <zephyr/drivers/spi.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/sys/util.h>
+
+#if !DT_NODE_EXISTS(DT_ALIAS(mux))
+#error "Multiplexer device node with alias 'mux' not defined."
+#endif
+
+#if !DT_NODE_EXISTS(DT_ALIAS(led_strip))
+#error "LED strip device node with alias 'led_strip' not defined."
+#endif
 
 LOG_MODULE_REGISTER(led_display, LOG_LEVEL_DBG);
 
-#define STRIP_NUM_PIXELS DT_PROP(DT_NODELABEL(led_strip), chain_length)
-#define PIXELS_PER_DISPLAY 128
-#define ROW_LENGTH 16
-#define NUM_ROWS (PIXELS_PER_DISPLAY / ROW_LENGTH)
+#define STRIP_NUM_PIXELS DT_PROP(DT_ALIAS(led_strip), chain_length)
 
 #define LED(_color, _brightness)                                    \
   {                                                                 \
@@ -20,36 +28,37 @@ LOG_MODULE_REGISTER(led_display, LOG_LEVEL_DBG);
   }
 
 static const uint32_t colors[] = {
-    0x7E0A6D, 0x00467E, 0x00467E, 0x00467E, 0xFF0000};
+    0x7E0A6D, 0x00467E, 0x00467E, 0x00467E, 0xFF0000
+};
 
-/** 4 x 7 digit binary pixel map */
-static const uint32_t digit_pixel_map_x[] = {
-    0xF99999F0, 0x26222270, 0xF11F88F0, 0xF11F11F0, 0x999F1110,
-    0xF88F11F0, 0xF88F99F0, 0xF1111110, 0xF99F99F0, 0xF99F11F0};
+/** 7 segment binary pixel map */
+static const uint8_t digit_segment_map[] = {0x7E, 0x30, 0x6D, 0x79, 0x33,
+                                            0x5B, 0x5F, 0x70, 0x7F, 0x7B};
 
-struct led_rgb pixels[STRIP_NUM_PIXELS];
-
-static const struct device *const strip =
-    DEVICE_DT_GET(DT_NODELABEL(led_strip));
+static struct led_rgb pixels[STRIP_NUM_PIXELS];
+static const struct device *const strip = DEVICE_DT_GET(DT_ALIAS(led_strip));
+static const struct device *const mux = DEVICE_DT_GET(DT_ALIAS(mux));
 
 static int display_digit(
     size_t display, uint8_t brightness, size_t offset, size_t digit
 ) {
   int rc;
-
+  size_t seg_offset = 0;
   struct led_rgb color = LED(colors[display], brightness);
 
-  uint32_t digit_map = digit_pixel_map_x[digit];
+  for (size_t segment = 0; segment < 7; segment++) {
+    if (0b01000000 & (digit_segment_map[digit] << segment)) {
+      /* Panel 1 */
+      memcpy(&pixels[0 + seg_offset + offset], &color, sizeof(struct led_rgb));
+      memcpy(&pixels[1 + seg_offset + offset], &color, sizeof(struct led_rgb));
+      memcpy(&pixels[2 + seg_offset + offset], &color, sizeof(struct led_rgb));
 
-  for (size_t cursor = 0; cursor < NUM_ROWS; cursor++) {
-    for (size_t column = 0; column < 4; column++) {
-      if (digit_map & (1UL << (31 - (cursor * 4) - column))) {
-        memcpy(
-            &pixels[(cursor * ROW_LENGTH) + offset + column], &color,
-            sizeof(struct led_rgb)
-        );
-      }
+      /* Panel 2 */
+      memcpy(&pixels[63 + seg_offset + offset], &color, sizeof(struct led_rgb));
+      memcpy(&pixels[64 + seg_offset + offset], &color, sizeof(struct led_rgb));
+      memcpy(&pixels[65 + seg_offset + offset], &color, sizeof(struct led_rgb));
     }
+    seg_offset += 3;
   }
 
   rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
@@ -62,41 +71,32 @@ static int display_digit(
 }
 
 int write_num_to_display(size_t display, uint8_t brightness, unsigned int num) {
-  if (!device_is_ready(strip)) {
-    LOG_ERR("LED strip device %s is not ready", strip->name);
-    return 1;
+  if (mux_set_active_port(mux, (uint8_t)display)) {
+    LOG_ERR("Failed to set correct mux channel");
+    return -1;
   }
 
-  size_t offset = display * PIXELS_PER_DISPLAY;
-
   // Turn off all pixels for the given display
-  memset(&pixels[offset], 0, sizeof(struct led_rgb) * PIXELS_PER_DISPLAY);
-
-  // for (size_t side = 0; side < 2; side++) {
-  //   offset += (side * PIXELS_PER_DISPLAY);
+  memset(&pixels[0], 0, sizeof(struct led_rgb) * STRIP_NUM_PIXELS);
 
   if (num > 999) {
     led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
   } else if (num > 99) {
-    display_digit(display, brightness, offset + 11, num % 10);
-    display_digit(display, brightness, offset + 6, num / 10 % 10);
-    display_digit(display, brightness, offset + 1, num / 100 % 10);
+    display_digit(display, brightness, 0, num % 10);
+    display_digit(display, brightness, 21, num / 10 % 10);
+    display_digit(display, brightness, 42, num / 100 % 10);
   } else if (num > 9) {
-    display_digit(display, brightness, offset + 8, num % 10);
-    display_digit(display, brightness, offset + 3, num / 10 % 100);
+    display_digit(display, brightness, 0, num % 10);
+    display_digit(display, brightness, 21, num / 10 % 100);
   } else {
-    display_digit(display, brightness, offset + 6, num);
+    display_digit(display, brightness, 0, num);
   }
-  // }
 
   return 0;
 }
 
 int turn_display_off(size_t display) {
-  memset(
-      &pixels[display * PIXELS_PER_DISPLAY], 0,
-      sizeof(struct led_rgb) * PIXELS_PER_DISPLAY
-  );
+  memset(&pixels[0], 0, sizeof(struct led_rgb) * STRIP_NUM_PIXELS);
   if (led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS) != 0) {
     LOG_ERR("Failed to update LED strip, test: %d", display);
   }
@@ -104,25 +104,39 @@ int turn_display_off(size_t display) {
 }
 
 #ifdef CONFIG_DEBUG
-void led_test_patern() {
-  static const uint32_t colors[] = {0xff0000, 0x00ff00, 0x0000ff, 0xffffff};
-  uint8_t brightness = 0x55;
+void led_test_patern(void) {
+  if (!device_is_ready(strip)) {
+    LOG_ERR("LED strip device %s is not ready", strip->name);
+  }
 
-  for (size_t test = 0; test < 4; test++) {
-    struct led_rgb pixel = LED((uint32_t)colors[test], (uint8_t)brightness);
-    // struct led_rgb pixel = {
-    //     .r = (((uint8_t)(colors[test] >> 16) * 0x55) >> 8),
-    //     .g = (((uint8_t)(colors[test] >> 8) * 0x55) >> 8),
-    //     .b = (((uint8_t)colors[test] * 0x55) >> 8)};
-    memset(&pixels[0], 0, sizeof(struct led_rgb) * STRIP_NUM_PIXELS);
-    k_msleep(10);
-    for (size_t cursor = 0; cursor < STRIP_NUM_PIXELS; cursor++) {
-      memcpy(&pixels[cursor], &pixel, sizeof(struct led_rgb));
+  if (!device_is_ready(mux)) {
+    LOG_ERR("MUX device %s is not ready", mux->name);
+  }
+
+  uint8_t brightness = 0x33;
+  uint32_t color = 0xFFFFFF;
+  struct led_rgb pixel = LED(color, brightness);
+
+  memset(&pixels[0], 0, sizeof(struct led_rgb) * STRIP_NUM_PIXELS);
+  for (size_t test = 0; test < (STRIP_NUM_PIXELS / 2); test++) {
+    memcpy(&pixels[test], &pixel, sizeof(struct led_rgb));
+    memcpy(&pixels[63 + test], &pixel, sizeof(struct led_rgb));
+
+    for (size_t i = 0; i < ARRAY_SIZE(colors); i++) {
+      mux_set_active_port(mux, i);
+      if (led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS) != 0) {
+        LOG_ERR("Failed to update LED strip, test: %d", 0);
+        return;
+      }
+    }
+    k_msleep(500);
+  }
+
+  for (size_t test = 0; test < 10; test++) {
+    for (size_t i = 0; i < ARRAY_SIZE(colors); i++) {
+      write_num_to_display(i, brightness, 111 * test);
     }
     k_msleep(1000);
-    if (led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS) != 0) {
-      LOG_ERR("Failed to update LED strip, test: %d", test);
-    }
   }
 }
 #endif
--- app/src/led_display.h
+++ app/src/led_display.h
@@ -4,14 +4,13 @@
 
 #ifndef LED_DISPLAY_H
 #define LED_DISPLAY_H
-#include <sys/_stdint.h>
-#include <sys/_types.h>
+#include <zephyr/types.h>
 
 int write_num_to_display(size_t display, uint8_t brightness, unsigned int num);
 
 int turn_display_off(size_t display);
 
 #ifdef CONFIG_DEBUG
-void led_test_patern();
+void led_test_patern(void);
 #endif
 #endif
