8e2a78b4711845e8332ff534f240554527b2f05d
jforgue <26842354+bandogora@users.noreply.github.com>
2024-12-29 16:38:27 -0500

    Refactor json_helpers to not use printk
    - Move helper functions to source file instead of header file
      - Fits with the rest of the project layout and allows us to use the
        logging module
    - Const functions instead of inline
    - Use logging statements instead of printk
      - Release builds no longer have printk

--- app/src/json/json_helpers.c
+++ app/src/json/json_helpers.c
@@ -1,9 +1,39 @@
 /** @headerfile json_helpers.h */
 #include "json/json_helpers.h"
 
-/** Compares a string with a jsmn token value. */
-extern inline int jsoneq(
-    const char *const json_ptr, const jsmntok_t *tok, const char *const string
-);
+#include <string.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 
-extern inline int eval_jsmn_return(int ret);
+LOG_MODULE_REGISTER(json_helpers);
+
+/** Compares a string with a jsmn token value. */
+const int jsoneq(const char *const json_ptr, const jsmntok_t *tok, const char *const string) {
+  if (tok->type == JSMN_STRING && (int)strlen(string) == tok->end - tok->start &&
+      strncmp(json_ptr + tok->start, string, tok->end - tok->start) == 0) {
+    return 1;
+  }
+  return 0;
+}
+
+const int eval_jsmn_return(const int ret) {
+  switch (ret) {
+    case JSMN_ERROR_NOMEM:
+      LOG_ERR("Failed to parse JSON; Not enough tokens were provided.\n");
+      return 1;
+    case JSMN_ERROR_INVAL:
+      LOG_ERR("Failed to parse JSON; Invalid character inside JSON string.\n");
+      return 2;
+    case JSMN_ERROR_PART:
+      LOG_ERR(
+          "Failed to parse JSON; The string is not a full JSON packet, more "
+          "bytes expected.\n"
+      );
+      return 3;
+    case 0:
+      LOG_WRN("Parsed Empty JSON string.\n");
+      return 4;
+    default:
+      return 0;
+  }
+}
